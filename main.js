8bef69c19a75779260b6163f8c727ff0711cb3ec
askerovtamerlan <tomcool@yandex.ru>
2024-02-20 21:22:53 +0300

    update style

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaG9va2FobG9jYXRvciIsImEiOiI5WnJEQTBBIn0.DrAlI
 const map = new mapboxgl.Map({
     container: 'map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: "mapbox://styles/hookahlocator/clsox9viv009f01pk1mf40pwu",
+    // style: "mapbox://styles/hookahlocator/clsox9viv009f01pk1mf40pwu",
+    // style: "mapbox://styles/mapbox/standard",
+    style: "mapbox://styles/hookahlocator/clsumxbp8002g01pihdv4290g",
     zoom: 11,
     center: [37.38888358146497,
         55.848076793921535],
@@ -22,18 +24,21 @@ const pointColor = '#eb4034'
 const start = [
     37.38888358146497,
     55.848076793921535
-  ];
+];
 const middle = [
     37.55197504508047,
     55.691508309624055
-  ];
+];
 const end = [
     37.85272076957557,
     55.82035077406326
-  ];
+];
 const coordinates = []
 coordinates.push(start, middle, end)
-const multiPoint = turf.multiPoint(coordinates)
+let coordinatesUpdated = coordinates.map((coordinate, index) => turf.point(coordinate, { id: index + 1 }))
+const multiPoint = turf.featureCollection(coordinatesUpdated)
+
+console.log(multiPoint)
 
 let fetchURL = ``
 coordinates.forEach((coordinate, index) => {
@@ -43,11 +48,31 @@ coordinates.forEach((coordinate, index) => {
     }
 })
 
-fetchURL = `https://api.mapbox.com/directions/v5/mapbox/cycling/` + fetchURL + `?steps=true&geometries=geojson&access_token=` + `${mapboxgl.accessToken}`
+fetchURL = `https://api.mapbox.com/directions/v5/mapbox/driving/` + fetchURL + `?steps=true&geometries=geojson&access_token=` + `${mapboxgl.accessToken}`
 
 console.log(fetchURL)
 
+async function getIconImage() {
+    map.loadImage('./square.png', (error, image) => {
+        if (error) throw error;
+        map.addImage('square', image)
+        console.log(image)
 
+        map.addLayer({
+            'id': 'points-image',
+            'type': 'symbol',
+            'source': {
+                type: 'geojson',
+                data: multiPoint
+            }, // reference the data source
+            'layout': {
+                'icon-image': "square", // reference the image
+                'icon-size': 0.4,
+                'icon-allow-overlap': true
+            }
+        }, 'points-text');
+    })
+}
 // create a function to make a directions request
 async function getRoute(start, end) {
     // make a directions request using cycling profile
@@ -71,7 +96,7 @@ async function getRoute(start, end) {
     };
 
     // otherwise, we'll make a new request
-    map.addLayer( {
+    map.addLayer({
         id: 'route',
         type: 'line',
         source: {
@@ -84,10 +109,10 @@ async function getRoute(start, end) {
         },
         paint: {
             'line-color': routeColor,
-            'line-width': 2,
+            'line-width': 6,
             'line-opacity': 0.85
         }
-    }, 'point-s');
+    }, 'points-text');
 
     // add turn instructions here at the end
 }
@@ -95,34 +120,83 @@ async function getRoute(start, end) {
 map.on('load', () => {
     // make an initial directions request that
     // starts and ends at the same location
+
+
+    // coordinates.forEach(coordinate => {
+    //     var el = document.createElement('div');
+    //     el.className = 'marker';
+    //     el.style.backgroundImage = 'url(/square.png)';
+    //     el.style.width = '16px';
+    //     el.style.height = '16px';
+
+    //     // add marker to map
+    //     new mapboxgl.Marker(el)
+    //         .setLngLat(coordinate)
+    //         .addTo(map);
+    // })
+
+
+    map.addSource('points', {
+        'type': 'geojson',
+        'data': multiPoint
+    });
+
+
+
+    map.addLayer({
+        id: 'points-text',
+        type: 'symbol',
+        source:
+            // type: 'geojson',
+            "points"
+        ,
+        layout: {
+            'text-field': ['get', 'id'],
+            'text-size': 15,
+            'text-offset': [-0.04, -0.01],
+            // 'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
+            'text-font': ['Open Sans Bold'],
+            // 'text-color': "#000000",
+            // 'text-justify': 'auto',
+            // 'text-allow-overlap': true
+        },
+        paint: {
+            'text-color': "#FFFFFF"
+        }
+    });
+
+    getIconImage();
     getRoute(start, end);
 
-    // Add starting point to the map
-    map.addLayer({
-        id: 'point-m',
-        type: 'circle',
-        source: {
-            type: 'geojson',
-            data: multiPoint
-        },
-        paint: {
-            'circle-radius': 6,
-            'circle-color': pointColor
-        }
-    });
 
-    map.addLayer({
-        id: 'point-s',
-        type: 'circle',
-        source: {
-            type: 'geojson',
-            data: multiPoint
-        },
-        paint: {
-            'circle-radius': 3,
-            'circle-color': "#fdf334"
-        }
-    });
+    // Add starting point to the map
+    // map.addLayer({
+    //     id: 'point-m',
+    //     type: 'circle',
+    //     source: {
+    //         type: 'geojson',
+    //         data: multiPoint
+    //     },
+    //     paint: {
+    //         'circle-radius': 6,
+    //         'circle-color': pointColor
+    //     }
+    // });
+
+    // map.addLayer({
+    //     id: 'point-s',
+    //     type: 'circle',
+    //     source: {
+    //         type: 'geojson',
+    //         data: multiPoint
+    //     },
+    //     paint: {
+    //         'circle-radius': 3,
+    //         'circle-color': "#fdf334"
+    //     }
+    // });
+
+
     // this is where the code from the next step will go
 });
 
